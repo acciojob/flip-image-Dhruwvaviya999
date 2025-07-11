adf97e755903f2ea1f077569600515d9ab7985db
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-07-12 00:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // write js code if required
+
+const image = document.querySelector(".flip");
+
+image.addEventListener("mouseover", (e) => {
+	e.target.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg";
+})
+
+image.addEventListener("mouseout", (e) => {
+	e.target.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg";
+})
